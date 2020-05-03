b7ad430fe5df6fe2a5c039ae0edef828c4f6e0b2
Teti <teti@gmail.com>
2020-05-03 16:18:43 -0300

    Test celdas entre 1 y 90

--- test/test_celdas.py
+++ test/test_celdas.py
@@ -1,13 +1,13 @@
 from src.bingo import carton
 
 def test_contar_celdas_ocupadas():
-	mi_carton = carton()
-	celdas_ocupadas = 0
-	for fila in mi_carton:
-		for celda in fila:
-			celdas_ocupadas += celda
+    mi_carton = carton()
+    celdas_ocupadas = 0
+    for fila in mi_carton:
+	for celda in fila:
+	    celdas_ocupadas += celda
 	
-	assert celdas_ocupadas == 15
+    assert celdas_ocupadas == 15
 
 
 
