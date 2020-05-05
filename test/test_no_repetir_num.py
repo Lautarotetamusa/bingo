018d403b97a6dd4fd908dce660ff4fb2d0a88a61
Teti <teti@gmail.com>
2020-05-05 13:42:38 -0300

    arreglando el test no repetir num(esta bien feo)

--- test/test_no_repetir_num.py
+++ test/test_no_repetir_num.py
@@ -1,14 +1,18 @@
 def test_no_repetir_num():
-    carton = ((11,25,31,45,56,67,78,89,90),
-              (10,54,35,87,95,85,99,53,12),
+    carton = ((1111111111111111111111,9,45,56,67,78,89,90),
+              (7,54,35,87,95,85,99,53,12),
               (14,16,17,18,19,20,22,23,46)
             )
     fil = 3
     col = 9 
+    numDif = 0
 
     for i in range (0, fil):
         for j in range(0, col):
             for ii in range (i, fil):
-                for jj in range(j+1, col):
-                    assert carton[i][j] <> carton[ii][jj]
+                for jj in range(j, col):
+                    if carton[i][j] != carton[ii][jj]:
+                        numDif += 1
+                            
+    assert numDif == (fil * col + 1)
 
