340f93c6b7f9b66ecc9f9d30370b8fd9e2434b29
Teti <teti@gmail.com>
2020-06-04 12:08:32 -0300

    funcion columna q devuelve una columna del carton

--- src/bingo.py
+++ src/bingo.py
@@ -7,6 +7,12 @@ def carton():
 	)
     return carton
 
+def columna(carton, nro_columna):
+    return(
+        carton[0][nro_columna],
+        carton[1][nro_columna],
+        carton[2][nro_columna]
+    )
 
 def carton_test_filas_vacias():
     carton = (
