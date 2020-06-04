85c25be05ba1ecf515667932ecd5e2fa957f34f6
Teti <teti@gmail.com>
2020-06-04 13:09:44 -0300

    arreglamos los teste de columnas y filas prdenadas para queno tomen en cuenta las celdas vacias representadas con 0

--- test/test_col_ordenadas.py
+++ test/test_col_ordenadas.py
@@ -1,8 +1,27 @@
+from src.bingo import columna
+
 def test_col_ordenadas():
-    carton = ((1,2,3,4,5,6,7,8,9,10),
-              (11,12,13,14,15,16,17,18,19),
-              (20,21,22,23,24,25,26,27,28)
+    carton = ((2,2,3,4,5,6,7,8,9,10),
+              (0,12,13,14,15,16,17,18,19),
+              (0,21,22,23,24,25,26,27,28)
               )
 
     for i in range(0, 9):
-        assert carton[1][i] < carton [2][i] and carton[0][i] < carton[1][i] 
+        col = columna(carton, i)
+        c = 0
+        
+        #contamos cuantas celdas vacias hay
+        for j in range(0, 3):
+            if col[j] == 0:
+                c += 1
+    
+        #si la cant de celdas vacias no es 2
+        if c != 2:    
+            if col[1] == 0:             #cuando la 2da casilla esta vacia
+                assert col[2] > col[0]
+            elif col[2] == 0:           #cuando la ultima casilla esta vacia
+                assert col[1] > col[0]
+            else:                       #cuando no hay ninguna vacia
+                assert col[2] > col[1] and col[1] > col[0] and col[2] > col[0]
+        else:
+            assert True
--- test/test_ord_filas.py
+++ test/test_ord_filas.py
@@ -1,5 +1,5 @@
 def test_ord_filas():
-    carton = ((1,2,3,4,5,6,7,8,9,10),
+    carton = ((0,1,3,4,5,0,0,8,9,0),
               (11,12,13,14,15,16,17,18,19),
               (20,21,22,23,24,25,26,27,28)
               )
@@ -7,7 +7,8 @@ def test_ord_filas():
     for j in range(0, 3):
         comparar = carton[j][0]
         for i in range(0, 8):
-            assert carton[j][i] < carton[j][i+1]
+            if carton[j][i+1] != 0:
+                assert carton[j][i] < carton[j][i+1]
             
 
     
