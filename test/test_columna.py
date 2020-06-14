228401c7904f97683c72a17217db3ce5de2c49b0
Teti <teti@gmail.com>
2020-06-14 15:29:16 -0300

    nuevo test para que solo haya 3 columnas con una celda

--- test/test_columna.py
+++ test/test_columna.py
@@ -5,11 +5,26 @@ carton = ((0,1,3,4,5,0,0,8,9),
          )
 
 
+carton_test_tres = ((0,1,3,4,5,4,0,8,9),
+                    (1,2,0,4,0,6,0,8,0),
+                    (0,0,2,0,0,0,6,0,8)
+                     )
+
+
+
 def vacia(c):
     return (c[0] == 0 and c[1] == 0 and c[2] == 0)
 
 def llena(c):
     return (c[0] != 0 and c[1] != 0 and c[2] != 0)
+
+def cant_celdas(c):
+    cant = 0
+    for i in c:
+        if i != 0:
+            cant = cant + 1
+
+    return cant
 
 def test_no_vacias():
     for i in range(0, 9):
@@ -19,3 +34,13 @@ def test_no_vacias():
 def test_no_3_ocupadas():
     for i in range(0, 9):
         assert llena(columna(carton, i)) == False
+
+
+def test_tres_con_una_celda():
+    cont = 0
+    for i in range(0, 9):
+        c = columna(carton_test_tres, i)
+        if cant_celdas(c) == 1:
+            cont = cont + 1
+
+    assert cont == 3
